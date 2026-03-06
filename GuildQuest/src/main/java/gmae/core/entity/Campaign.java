18b40e0b61502dcb3b9f74841ba727f18222c9d3
Dalton Denis <ddenis@uci.edu>
2026-03-06 15:05:24 -0800

    Filled out Campaign.java and User.java, should be functional moving forward.

--- GuildQuest/src/main/java/gmae/core/entity/Campaign.java
+++ GuildQuest/src/main/java/gmae/core/entity/Campaign.java
@@ -1,4 +1,56 @@
 package gmae.core.entity;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Campaign {
+    private UUID campaignId;
+    private String name;
+    private boolean isPublic;
+    private boolean isArchived;
+    private User ownerUser;
+    private ArrayList<QuestEvent> questEvents;
+
+    public Campaign(String name, boolean isPublic, User owner, ArrayList<QuestEvent> events) {
+        this.campaignId = UUID.randomUUID();
+        this.name = name;
+        this.isPublic = isPublic;
+        this.ownerUser = owner;
+        this.questEvents = new ArrayList<>();
+        this.questEvents.addAll(events);
+    }
+
+    public UUID getCampaignId() {
+        return campaignId;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isPublic() {
+        return isPublic;
+    }
+    public boolean isArchived() {
+        return isArchived;
+    }
+    public User getOwnerUser() {
+        return ownerUser;
+    }
+    public ArrayList<QuestEvent> getQuestEvents() {
+        return questEvents;
+    }
+
+    public void addQuestEvent(QuestEvent questEvent) {
+        this.questEvents.add(questEvent);
+    }
+    public void removeQuestEvent(QuestEvent questEvent) {
+        UUID id = questEvent.getEventId();
+        questEvents.removeIf(q -> q.getEventId().equals(id));
+    }
+
+    public void toggleVisibility() {
+        isPublic = !isPublic;
+    }
+    public void archiveCampaign() {
+        isArchived = true;
+    }
 }
--- GuildQuest/src/main/java/gmae/core/entity/User.java
+++ GuildQuest/src/main/java/gmae/core/entity/User.java
@@ -1,4 +1,45 @@
 package gmae.core.entity;
 
+import jdk.jfr.EventSettings;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class User {
+    private final UUID userId;
+    private String username;
+    private ArrayList<Campaign> ownedCampaigns;
+
+    public User(String username) {
+        this.userId = UUID.randomUUID();
+        this.username = username;
+        this.ownedCampaigns = new ArrayList<>();
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public ArrayList<Campaign> getOwnedCampaigns() {
+        return ownedCampaigns;
+    }
+
+    public void addCampaign(Campaign campaign) {
+        if(campaign != null && !ownedCampaigns.contains(campaign)) {
+            ownedCampaigns.add(campaign);
+        }
+    }
+    public void removeCampaign(Campaign campaign) {
+        if(ownedCampaigns.contains(campaign)) {
+            ownedCampaigns.remove(campaign);
+        }
+        else {
+            System.out.println("User: " + username + " does not own campaign: " + campaign.getName());
+        }
+    }
+    public boolean ownsCampaign(Campaign campaign) {
+        return ownedCampaigns.contains(campaign);
+    }
 }
