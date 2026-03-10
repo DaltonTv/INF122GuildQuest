e94fabe02508bac6d694a01b878637d37f98e926
Dalton Denis <ddenis@uci.edu>
2026-03-10 14:29:25 -0700

    Added a GridPosition class for portability and access, fixed typo in PlayerInput (fronString -> fromString), removed unused import from User, initialized totalLosses in PlayerProfile

---
+++ GuildQuest/src/main/java/gmae/core/entity/GridPosition.java
@@ -0,0 +1,25 @@
+package gmae.core.entity;
+
+public class GridPosition {
+    private int x;
+    private int y;
+
+    public GridPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
